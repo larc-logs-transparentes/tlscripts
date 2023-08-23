4f3f3bf2bde999b1f786d39dd7559b49ac8da9c1
gfumagali <guilhermefumarques@gmail.com>
2023-08-23 20:28:07 -0300

    [FIX] file read in batch

--- recont/json_utils.py
+++ recont/json_utils.py
@@ -2,7 +2,7 @@ import ijson
 import json
 
 def get_json_data(filename):
-    with open(filename, 'r') as f:
-        objects = ijson.items(f, 'item')
-        rows = list(objects)
-    return rows
+    f = open(filename, 'rb')
+    objects = ijson.items(f, 'item')
+    rows = (o for o in objects)
+    return f, rows
--- recont/recont.py
+++ recont/recont.py
@@ -2,20 +2,8 @@ from bu_class import BU
 from json_utils import get_json_data
 from bu_functions import soma_votos
 
-bus_json = get_json_data('recont/bu_1t.json')
-bu = BU(bus_json[0])
-print(bu.get_dados_secao())
-
-resultados = bu.get_resultados_por_eleicao()
-
-for eleicao_type in resultados.eleicoes:
-    print(eleicao_type.id_eleicao)
-    for cargo in eleicao_type.resultados.keys():
-        print(cargo)
-        for codigo_candidato in eleicao_type.resultados[cargo].keys():
-            print(f'{codigo_candidato}: {eleicao_type.resultados[cargo][codigo_candidato]}')
-    print('-----------------------')
-     
+f, bus_json = get_json_data('recont/results/bu_imgbu_logjez_rdv_vscmr_2022_1t_SP.json')
 resultado_presidente = soma_votos(bus_json).soma_por_cargo['presidente']
 for codigo_presidente in resultado_presidente.keys():
     print(f'{codigo_presidente}: {resultado_presidente[codigo_presidente]}')
+f.close()
