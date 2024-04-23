2eeb45b412d56af6b301e2b7ea786c91ff1bfc72
leonardotkimura <leonardotkimura@gmail.com>
2024-04-23 15:40:37 -0300

    add timer in script

--- bu_full_verification.py
+++ bu_full_verification.py
@@ -4,10 +4,12 @@ from termcolor import cprint
 from tl_preprocessor.src.bu_preprocessor import preprocess_bus
 from tl_verifier.src.verify_bus import verify_tree
 import json
+import datetime
 from tl_sum.src.service.json_utils import get_json_data_from_file, get_json_data_from_dir, print_dict
 from tl_sum.src.bu_functions import soma_votos
 from pathlib import Path
 # from tl_sum.tl_sum 
+
 
 
 
@@ -19,27 +21,45 @@ tree_name = ask_user_which_election()
 
 cprint('\n\nPasso 1: Baixar os arquivos BUs da eleição', 'green')
 print(f'Iniciando download da eleição {tree_name}')
+start_time = datetime.datetime.now()
 download_bu(tree_name)
+end_time = datetime.datetime.now()
 print('Download finalizado')
+print(end_time - start_time)
+
+
 
 cprint('\n\nPasso 2: Verificar os arquivos BUs', 'green')
+start_time = datetime.datetime.now()
 result = verify_tree(tree_name)
+end_time = datetime.datetime.now()
 print(json.dumps(result, indent=4))
+print(end_time - start_time)
+
+
+
 
 cprint('\n\nPasso 3: Preprocessar os arquivos BUs', 'green')
+start_time = datetime.datetime.now()
 preprocess_bus('./res/trees/' + tree_name, './res/preprocessed_bu_jsons/' + tree_name)
+end_time = datetime.datetime.now()
 cprint('\nPreprocessamento finalizado')
+print(end_time - start_time)
+
+
 
 cprint('\n\nPasso 4: Calcular o resultado da eleição', 'green')
 cargo = input("Digite o cargo: ")
-
-
+start_time = datetime.datetime.now()
 files, bus_json = get_json_data_from_dir(Path('./res/preprocessed_bu_jsons/' + tree_name))
 resultado = soma_votos(bus_json, cargo)
 for f in files:
     f.close()
-
 print_dict(resultado)
+end_time = datetime.datetime.now()
+print(end_time - start_time)
+
+
 
 cprint('\n\nFim do script', 'green')
 
